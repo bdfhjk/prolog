6188b23ac56b755c68f13b301d9dc0efb248229e
Marek Bardoński <bdfhjk@gmail.com>
2016-06-05 16:38:12 +0200

    zadanie + testy

--- zad/mb336912.pl
+++ zad/mb336912.pl
@@ -15,8 +15,11 @@
 % ------------------------------- [UTILS] -----------------------------------
 % ---------------------------------------------------------------------------
 
-% fill(List, Element, Number_of_elements)
-% Create a list with n elements equal E.
+% Define operatior <> for the purpose of the syntax parsing.
+:- op(700,xfx,[<>]).
+
+% fill_increasing(List, N)
+% Create a list with N elements equal 0,1,...,N-1.
 fill_increasing([], 0).
 
 fill_increasing([H|T], N) :-
@@ -24,10 +27,6 @@ fill_increasing([H|T], N) :-
   N2 is N - 1,
   H is N - 1,
   fill_increasing(T, N2).
-
-generateNumbers(N, X) :-
-  fill_increasing(T, N),
-  member(X, T).
 
 % fill(List, Element, Number_of_elements)
 % Create a list with n elements equal E.
@@ -38,16 +37,11 @@ fill([H|T], X, N) :-
   H = X,
   fill(T, X, N2).
 
-fill([H], X, 1) :- X = H.
-
 % increase_nth(Id, List_in, List_out)
 % increase id'th element in the integer list by one.
 increase_nth(0, [H|T], [H2|T2]) :-
   H2 is H + 1,
   T = T2.
-
-increase_nth(0, [H], [H2]) :-
-  H2 is H + 1.
 
 increase_nth(Id, [H|T], [H2|T2]) :-
   H = H2,
@@ -60,9 +54,6 @@ set_nth(0, E, [_|T], [H2|T2]) :-
   H2 = E,
   T = T2.
 
-set_nth(0, E, [_], [H2]) :-
-  H2 = E.
-
 set_nth(Id, E, [H|T], [H2|T2]) :-
   H = H2,
   Id2 is Id - 1,
@@ -74,9 +65,6 @@ set_nth_2(0, NN, E, [H|T], [H2|T2]) :-
   set_nth(NN, E, H, H2),
   T = T2.
 
-set_nth_2(0, NN, E, [H], [H2]) :-
-  set_nth(NN, E, H, H2).
-
 set_nth_2(Id, NN, E, [H|T], [H2|T2]) :-
   H = H2,
   Id2 is Id - 1,
@@ -85,9 +73,6 @@ set_nth_2(Id, NN, E, [H|T], [H2|T2]) :-
 % get_nth_2(Id-1dim, Id-2dim, Element, List)
 % get [Id-1dim, Id-2dim] th element from 2 dimensional list and save to Element.
 get_nth_2(0, NN, E, [H|_]) :-
-  nth0(NN, H, E).
-
-get_nth_2(0, NN, E, [H]) :-
   nth0(NN, H, E).
 
 get_nth_2(Id, NN, E, [_|T]) :-
@@ -99,12 +84,17 @@ get_nth_2(Id, NN, E, [_|T]) :-
 % ----------------------------- [PRINTING UTILS] ----------------------------
 % ---------------------------------------------------------------------------
 
+
+% write_processes(IdentificatorsList, InstructionList)
+% write processes state according to the task specification
 write_processes([],[]).
 
 write_processes([H|T], [H2|T2]) :-
   format("    Proces ~d : ~d ~n", [H, H2]),
   write_processes(T, T2).
 
+% write_in_section(ProcessesList)
+% write processes in section numbers according to the task specification.
 write_in_section([H]) :-
   format("~d.~n", [H]).
 
@@ -116,6 +106,21 @@ write_in_section([H|T]) :-
 % ----------------------------- [MAIN CODE] ---------------------------------
 % ---------------------------------------------------------------------------
 
+% ide(N, Program, Numer, Odwiedzone,
+%     Kolejka, StanW, NumerW, ListaId, ListaNum, WSekcji)
+% N - number of processes.
+% Program - the analyzed program.
+% Numer - Number of a current state.
+% Odwiedzone - visited states ListaId.
+% Kolejka - queue with not processed states.
+% StanW - found unsafe states.
+% ListaId - processes numbers in unsafe state.
+% ListaNum - processes status counters in unsafe state.
+% WSekcji - numbers of processes in critical section.
+% BFS-like traversing all of the possible states with an unstructured order.
+% This function is getting the first state from the queue,
+% checking if it's an unsafe state and calling the pojde function to add all
+% non visited neighbors to the queue.
 ide(N, Program, Numer, _, [Stan|_], Stan, Numer, ListaId, ListaNum, WSekcji) :-
   wrong(N, Program, Stan, Numer, ListaId, ListaNum, WSekcji).
 
@@ -125,18 +130,20 @@ ide(N, Program, Numer, Odwiedzone, Kolejka, StanW, NumerW,
   pojde(N, Program, Numer, Odwiedzone, Kolejka, StanW, NumerW,
    ListaId, ListaNum, WSekcji, ListaP).
 
+% pojde(N, Program, Numer, Odwiedzone,
+%     Kolejka, StanW, NumerW, ListaId, ListaNum, WSekcjil, ProcessesToProces)
+% ProcessesToProces - numbers of processes to be analyzed.
+% A helper function to ide. It's getting all unvisited neighbors
+% of a current state and putting them into the queue.
 pojde(N, Program, Numer, Odwiedzone, [Stan|T], StanW, NumerW,
    ListaId, ListaNum, WSekcji,  [Id|T2]) :-
   step(Program, Stan, Id, StanN),
-  !,
-  proper_length(Odwiedzone, L),
-  format("Dlugosc odwiedzonych: ~d ~n", [L]),
+  !,      % Cut -> we don't want to analyze any different path to StanN.
   (member(StanN, Odwiedzone) ->
     pojde(N, Program, Numer, Odwiedzone, [Stan|T], StanW, NumerW,
       ListaId, ListaNum, WSekcji, T2);
     pojde(N, Program, Numer, [StanN|Odwiedzone], [Stan,StanN|T],
-      StanW, NumerW, ListaId, ListaNum, WSekcji, T2)),
-    format("Numer: ~d ~n", [Numer]).
+      StanW, NumerW, ListaId, ListaNum, WSekcji, T2)).
 
 pojde(N, Program, Numer, Odwiedzone, [_|T], StanW, NumerW,
  ListaId, ListaNum, WSekcji, []) :-
@@ -144,24 +151,28 @@ pojde(N, Program, Numer, Odwiedzone, [_|T], StanW, NumerW,
    ide(N, Program, Numer2, Odwiedzone, T, StanW, NumerW, ListaId,
     ListaNum, WSekcji).
 
+% Verificate if N processes of a program stored in file can produce an unsafe
+% state.
 verify(N, File) :-
   N > 0,
   set_prolog_flag(fileerrors, off),
   see(File),
-  !,
+  !, % Cut -> we don't want to process alternatives if we can open the file.
   read(Vars),
   read(Arrays),
   read(Program),
   seen,
   initState(Program, N, Vars, Arrays, StanP),
-  (ide(N, Program, 1, [StanP], [StanP], StanW, NumerW, ListaId,
+  (ide(N, Program, 1, [StanP], [StanP], _, NumerW, ListaId,
     ListaNum, WSekcji) ->
     format("Program jest niepoprawny: stan nr ~d nie jest bezpieczny.
 Nieporawny przeplot: ~n", [NumerW]),
     write_processes(ListaId, ListaNum),
     format("Procesy w sekcji: "),
-    write_in_section(WSekcji);
-    format("Program jest poprawny (bezpieczny).~n")).
+    write_in_section(WSekcji),
+    !;
+    format("Program jest poprawny (bezpieczny).~n")),
+    !.
 
 verify(N, File) :-
   N > 0,
@@ -170,6 +181,13 @@ verify(N, File) :-
 verify(_, _) :-
   format("Error: parametr 0 powinien byc liczba > 0").
 
+% Initial state creation
+% initState(Program, N, Vars, Arrays, StanP)
+% Program - the list of instructions in the program
+% N - number of exisitng processes
+% Vars - list of variables
+% Arrays - list of arrays
+% StanP - output state created from above variables.
 initState(_, N, Vars, Arrays, StanP) :-
   VT = Vars,
   AT = Arrays,
@@ -181,16 +199,19 @@ initState(_, N, Vars, Arrays, StanP) :-
   fill(V, 0, VTS),
   fill(AN, 0, N),
   fill(A, AN, ATS),
-  nb_setval(visited, []),
-  nb_setval(stan_number, 0),
   StanP = [C, VT, V, AT, A].
 
-
+% wrong(N, Program, Stan, _, ListaId, ListaNum, WSekcji)
+% Arguments have the same meaning as in ide(...)
+% The function return true and fill ListaId, ListaNum, WSekcji, if
+% Stan is an unsafe state.
 wrong(N, Program, Stan, _, ListaId, ListaNum, WSekcji) :-
   fill_increasing(ListaP, N),
   wrong(N, Program, Stan, Num, ListaId, ListaNum, WSekcji, ListaP),
-  Num > 1.
+  Num > 1.    % Return true only if we have more than two processes in section.
 
+% The wrong/8 paradigm is a helper function
+% similiar to wrong/7, the last argument is the list of processes to check.
 wrong(_, _, _, 0, [], [], [], []).
 
 wrong(N, Program, [C, VT, V, AT, A], Num2, ListaId, ListaNum, WSekcji,
@@ -206,6 +227,10 @@ wrong(N, Program, [C, VT, V, AT, A], Num2, ListaId, ListaNum, WSekcji,
     wrong(N, Program, [C, VT, V, AT, A], Num2, ListaIdR,
       ListaNumR, WSekcji, T)).
 
+% is_sekcja(Program, H)
+% Program - program to be analyzed
+% H - number of instruction in program.
+% return true if H-th instruction of Program is sekcja.
 is_sekcja(Program, H) :-
   Program = program(ProgramList),
   nth0(H, ProgramList, sekcja).
